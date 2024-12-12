b90bf5d624452253e9eef1700dd3c55c77a7d8dc
sofia alves toreti <sofia.a2005@aluno.ifsc.edu.br>
2024-12-12 20:14:14 -0300

    slide show vou me matar

--- js/depoimentos.js
+++ js/depoimentos.js
@@ -28,25 +28,63 @@ const depoimentos = [
     },
 ]
 
-depoimentos.forEach(depoimento => {
-    const conteiner = document.createElement('div');
-    conteiner.classList.add('depoimentos__conteiner');
+// depoimentos.forEach(depoimento => {
+//     const conteiner = document.createElement('div');
+//     conteiner.classList.add('depoimentos__conteiner');
 
-    const texto = document.createElement('p');
-    texto.classList.add('depoimentos__texto')
-    texto.textContent = depoimento.texto
+//     const texto = document.createElement('p');
+//     texto.classList.add('depoimentos__texto')
+//     texto.textContent = depoimento.texto
 
-    const autor = document.createElement('p');
-    autor.classList.add('depoimentos__autor')
-    autor.textContent = depoimento.autor
+//     const autor = document.createElement('p');
+//     autor.classList.add('depoimentos__autor')
+//     autor.textContent = depoimento.autor
 
-    conteiner.append(texto);
-    conteiner.append(autor);
+//     conteiner.append(texto);
+//     conteiner.append(autor);
 
-    conteudo.append(conteiner);
+//     conteudo.append(conteiner);
 
-});
+// });
+
+let slide = 0
+
+const conteiner = document.createElement('div');
+conteiner.classList.add('depoimentos__conteiner');
+
+const texto = document.createElement('p');
+texto.classList.add('depoimentos__texto')
+texto.textContent = depoimentos[slide].texto
+
+const autor = document.createElement('p');
+autor.classList.add('depoimentos__autor')
+autor.textContent = depoimentos[slide].autor
+
+conteiner.append(texto);
+conteiner.append(autor);
+
+const setaDireita = document.createElement('img')
+setaDireita.classList.add('depoimentos__seta');
+setaDireita.src = 'img/icons/next-rigght.png'
+
+const setaEsquerda = document.createElement('img')
+setaEsquerda.classList.add('depoimentos__seta');
+setaEsquerda.classList.add('sinistra');
+setaEsquerda.src = 'img/icons/next-rigght.png'
+
+
+conteudo.append(setaEsquerda);
+conteudo.append(conteiner);
+conteudo.append(setaDireita);
+
+
+
 
 const main = document.querySelector('main');
 
 main.lastElementChild.before(secao)
+
+
+function slideNext(slide) {
+    slide = (slide + 1) % depoimentos.length
+}
